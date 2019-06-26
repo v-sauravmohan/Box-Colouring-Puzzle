b0a167c17c3a0c4e3c8df0acd2c13d377287e801
Saurav Mohan V <sauravmohan2@gmail.com>
2019-06-26 22:45:22 +0530

    Initial Commit
    Need to add more information to the project.

---
+++ Col-Puzzle.py
@@ -0,0 +1,16 @@
+def minPrice(cost):
+    List = []
+    SubList = []
+    print("Min Price Funtion!!")
+    for i in range 3:
+        for i in range 3:
+            list
+            
+
+
+
+
+if __name__ == '__main__':
+    cost = [[2,2,1],[2,3,1],[2,1,1]]
+    result = minPrice(cost)
+    print(result)
